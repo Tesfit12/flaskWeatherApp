4cef8cff5069bff068642d599b24a78f4116bad6
Tesfit12 <menkregrma12@gmail.com>
2020-06-27 12:00:42 +0300

    Third Commit added table and css

--- app.py
+++ app.py
@@ -7,4 +7,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('weather.html')
+    key_code = '93373de30fe2fe54c87196440b0c1627'
+    city_name = 'Israel'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={key_code}'
+    r = requests.get(url).json()
+
+    weather_obj = {
+        'city': r['name'],
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+    }
+
+    return render_template('weather.html', weather_obj=weather_obj)
+
+     
